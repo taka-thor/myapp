b54fb99d744216154ab85d60822f79b77a385bdb
taka-thor <195444215+taka-thor@users.noreply.github.com>
2025-11-29 19:51:22 +0900

    user_controllerやuser_new.htmlを作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new;end
+
+  def create
+
+  #既存ユーザーであれば、redirect to home
+
+  end
+end
+
+
+
+
+#toppageから次へを押すと、新規ユーザーか既存ユーザーかを条件分岐
