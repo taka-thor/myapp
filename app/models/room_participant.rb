4e9e75185b55f9807bc90936c1f5a7eb6af7aa75
taka-thor <195444215+taka-thor@users.noreply.github.com>
2026-01-07 08:17:19 +0900

    fix: room_participantsモデルのクラスメソッドを修正した

--- app/models/room_participant.rb
+++ app/models/room_participant.rb
@@ -10,21 +10,18 @@ class RoomParticipant < ApplicationRecord
       .where("last_seen_at >= ?", ttl_seconds.seconds.ago) # ttl_seconds(=180).seconds(=秒).ago(=前) =>180秒前 その基準はTime.current
   }                                                        # >=?の?にttl_seconds.seconds.agoの値を渡している Time.currentから180秒以内の値があるレコードを選択
 
-  def self.enter!(room:, user:)
-    rp = find_or_initialize_by(room_id: room.id, user_id: user.id)
+class << self
+  def touch!(room:, user:)
+    rp = find_or_initialize_by(room_id: room.id, user_id: user.id) # 探す、なければ新規作成
     rp.is_active = true
     rp.last_seen_at = Time.current
     rp.save!
     rp
   end
 
-  def self.touch!(room:, user:) # ブラウザからheartbeatを取得後にDBを更新するメソッド
-    where(room_id: room.id, user_id: user.id)
-      .update_all(is_active: true, last_seen_at: Time.current)
-  end
-
-  def self.leave!(room:, user:)
+  def leave!(room:, user:)
     where(room_id: room.id, user_id: user.id)
       .update_all(is_active: false)
   end
+end
 end
