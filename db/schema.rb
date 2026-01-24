c26c536255688e46f63dc333df75973215a45d7c
taka-thor <195444215+taka-thor@users.noreply.github.com>
2026-01-24 12:22:50 +0900

    feat: room_@ar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_06_045201) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_24_030318) do
   create_table "ng_words", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -25,9 +25,11 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_06_045201) do
     t.datetime "last_seen_at"
     t.datetime "left_at"
     t.integer "room_id", null: false
+    t.string "session_id"
     t.datetime "updated_at", null: false
     t.integer "user_id", null: false
     t.index ["room_id"], name: "index_room_participants_on_room_id"
+    t.index ["session_id"], name: "index_room_participants_on_session_id"
     t.index ["user_id"], name: "index_room_participants_on_user_id"
     t.index ["user_id"], name: "index_room_participants_unique_active_user", unique: true, where: "is_active = 1"
   end
