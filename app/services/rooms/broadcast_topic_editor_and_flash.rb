e865533353b1d432a0a4014925f818f9bdcd1648
taka-thor <195444215+taka-thor@users.noreply.github.com>
2026-02-23 22:47:51 +0900

    updade

--- app/services/rooms/broadcast_topic_editor_and_flash.rb
+++ app/services/rooms/broadcast_topic_editor_and_flash.rb
@@ -2,27 +2,18 @@ class Rooms::BroadcastTopicEditorAndFlash
   def self.call(room:, flash:)
     flash_messages = flash.to_hash
 
-    streams = [
-      Turbo::StreamsChannel.turbo_stream_action_tag(
-        :replace,
-        target: "topic_editor",
-        template: ApplicationController.render(
-          partial: "rooms/topic_editor",
-          locals: { room: room },
-          formats: [ :html ]
-        )
-      ),
-      Turbo::StreamsChannel.turbo_stream_action_tag(
-        :replace,
-        target: "flash",
-        template: ApplicationController.render(
-          partial: "shared/flash_messages",
-          locals: { flash_messages: flash_messages },
-          formats: [ :html ]
-        )
-      )
-    ].join
+    Turbo::StreamsChannel.broadcast_replace_to(
+      room,
+      target: "topic_editor",
+      partial: "rooms/topic_editor",
+      locals: { room: room }
+    )
 
-    Turbo::StreamsChannel.broadcast_stream_to(room, content: streams)
+    Turbo::StreamsChannel.broadcast_replace_to(
+      room,
+      target: "flash",
+      partial: "shared/flash_messages",
+      locals: { flash_messages: flash_messages }
+    )
   end
 end
