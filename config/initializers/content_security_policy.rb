b79f613ef6933c9c5ea51486c115cec73e5d6145
taka-thor <195444215+taka-thor@users.noreply.github.com>
2026-02-27 16:35:32 +0900

    update: content_security_policyを設定

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,25 +1,26 @@
 # Be sure to restart your server when you modify this file.
-
+#
 # Define an application-wide content security policy.
 # See the Securing Rails Applications Guide for more information:
 # https://guides.rubyonrails.org/security.html#content-security-policy-header
 
-# Rails.application.configure do
-#   config.content_security_policy do |policy|
-#     policy.default_src :self, :https
-#     policy.font_src    :self, :https, :data
-#     policy.img_src     :self, :https, :data
-#     policy.object_src  :none
-#     policy.script_src  :self, :https
-#     policy.style_src   :self, :https
-#     # Specify URI for violation reports
-#     # policy.report_uri "/csp-violation-report-endpoint"
-#   end
-#
-#   # Generate session nonces for permitted importmap, inline scripts, and inline styles.
-#   config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
-#   config.content_security_policy_nonce_directives = %w(script-src style-src)
-#
-#   # Report violations without enforcing the policy.
-#   # config.content_security_policy_report_only = true
-# end
+Rails.application.configure do
+  config.content_security_policy do |policy|
+    policy.default_src :self, :https
+    policy.object_src  :none
+    policy.base_uri    :self
+    policy.frame_ancestors :none
+
+    policy.font_src  :self, :https, :data
+    policy.img_src   :self, :https, :data, :blob
+    policy.script_src :self, :https
+    # Existing views use inline <style>, so allow inline styles for now.
+    policy.style_src :self, :https, :unsafe_inline
+
+    if Rails.env.development?
+      policy.connect_src :self, :https, :ws, :wss
+    else
+      policy.connect_src :self, :https, :wss
+    end
+  end
+end
